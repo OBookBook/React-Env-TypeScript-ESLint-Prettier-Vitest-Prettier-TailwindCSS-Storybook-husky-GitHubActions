9b640b700f25de74cb1dbc7b318d9c8c55c0c6e3
N.I <iworksWeb@gmail.com>
2025-01-25 02:39:36 +0900

    add vitest #3

---
+++ src/components/TextInput.test.tsx
@@ -0,0 +1,20 @@
+import "@testing-library/jest-dom/vitest";
+import TextInput from "@/components/TextInput";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+
+test("TextInput Component Test", async () => {
+  const user = userEvent.setup(); // ユーザーイベントをシミュレートするためのセットアップ
+  render(<TextInput />);
+
+  // "Entered Text:"というテキストが画面に表示されていることを確認
+  const textElement = screen.getByText("Entered Text:");
+  expect(textElement).toBeInTheDocument();
+
+  // aria-labelが"Text Input"の入力フィールドを取得
+  const inputElement = screen.getByLabelText("Text Input");
+  // 入力フィールドに"Hello World"というテキストを入力
+  await user.type(inputElement, "Hello World");
+  // 入力したテキストが"Entered Text: Hello World"として表示されていることを確認
+  expect(screen.getByText("Entered Text: Hello World")).toBeInTheDocument();
+});
